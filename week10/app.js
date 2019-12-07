b181d6e45694393e1afc3ffce13360705eaace74
EC2 Default User <ec2-user@ip-172-31-30-253.ec2.internal>
2019-12-07 03:58:58 +0000

    updates for final

--- week10/app.js
+++ week10/app.js
@@ -4,33 +4,23 @@ var express = require('express'), // npm install express
     app = express();
 var handlebars = require('handlebars')
 var fs = require('fs')
-    
-var landingPage = `<h1>Welcome to my three projects!</h1>
-<ul>
-<li><a href="/sensor">Sensor Data.</a></li>
-<li><a href="/ProcessBlogAAY">Process Blog Data.</a></li>
-<li><a href="/aamap">AA Map Data.</a></li>
-</ul>
-`;
-
-
-// serve static files in /public
-app.use(express.static('public'));
-
-// listen on port 8080
-app.listen(8080, function() {
-    console.log('Server listening...');
-});
-
+var bodyParser = require('body-parser');
+var path = require('path');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var moment = require('moment');
+const { format, parseISO } =require('date-fns')
+var dir= __dirname;
 
 // Details for process blog data
 var AWS = require('aws-sdk');
 AWS.config = new AWS.Config();
 AWS.config.region = "us-east-1";
+var dynamodb = new AWS.DynamoDB();
 
 // Details for sensor data
 const { Client } = require('pg'); // npm install dotenv
 const dotenv = require('dotenv'); // npm install dotenv
+// const cTable = require('console.table');
 dotenv.config({path: '/home/ec2-user/environment/.env'});
 
 // AWS RDS POSTGRESQL INSTANCE
@@ -41,30 +31,61 @@ db_credentials.database = 'aa';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
+//setup
+// app.set('view engine', 'html');
+app.use(express.static(path.join(dir, '/public')));
+app.set('views', path.join(dir, 'public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
-// AA Map Data Request
+// AA MAP
+//render html file
 app.get('/aamap', function(req, res) {
-    var output = {title: 'AA Map Visualization', body: 'example content'};
+    res.sendFile(dir +"/public/aamap.html")
+    //res.render("aa.html")
+});
+
+//query data
+app.post('/aaData',  function(req, res) {
+// Connect to the AWS RDS Postgres database
+// var templateVariables= {};
 
     const client = new Client(db_credentials);
-        client.connect();
-    
-    
-    var thisAAQuery = "SELECT aa_location.lat, aa_location.long, aa_location.building FROM aa_location;";
-    //INNER JOIN aa_event ON aa_location.locationid = aa_event.locationid;
+    client.connect();
 
-    client.query(thisAAQuery, (err, results) => {
+    var queryDay= req.body.queryDay
+    // var [from, to]=req.body.queryTime.split(",");
+    var queryTime= req.body.queryTime
+    //queryTime= req.body.queryTime
+
+// look at this filtered query with Ryan. Do I need to go back and parse meeting name? 
+    var thisQuery = "SELECT lat, long, building, address json_agg(json_build_object('Day', day, 'Start Time', startTime,  'End Time', endTime, 'Meeting Type', meetingtype, 'Special Interest', specialinterest)) as meetings FROM aa_event JOIN aa_location ON aa_event.locationid = aa_location.locationid WHERE (1=1) " + queryDay +  queryTime + " GROUP BY latitude, longitude, address;";
+    client.query(thisQuery, (err, results) => {
         if (err) {throw err}
         else {
-            console.log(res.rows);
-            fs.readFile('./aamap.html', 'utf8', (error, data) => {
-                var template = handlebars.compile(data);
-                output.meetings = results.rows
-                var html = template(output);
-                res.send(html);
-            })
-            client.end();
+            const data = results.rows;
+            console.log(data)
+              client.end();
+              return res.json(data);
+        }
+    });
+});
+
+
+app.get('/aaDefault',  function(req, res) {
+
+const client = new Client(db_credentials);
+client.connect();
+
+    var thisQuery = "SELECT lat, long, address, building FROM aa_location ;";
+    client.query(thisQuery, (err, results) => {
+        if (err) {throw err}
+        else {
+            const data = results.rows;
+            // console.log(data)
+              client.end();
+              return res.json(data);
         }
     });
 });
@@ -72,11 +93,47 @@ app.get('/aamap', function(req, res) {
 
 
 
-// Process Blog
-app.get('/blog', function(req, res) {
-    var output = {title: 'Process Blog', body: 'example content'};
-    var dynamodb = new AWS.DynamoDB();
+
+
+// Attempt 1: 
+// Query Functions:
+// AA Map:
+// app.get('/aamap', function (req, res) {
     
+    // const client = new Client(db_credentials);
+    // client.connect();
+
+    // var thisAAQuery;
+    // thisAAQuery = "SELECT aa_location.lat, aa_location.long, aa_location.building FROM aa_location";
+    // thisAAQuery +=  "INNER JOIN aa_location ON aa_event.locationid=aa_location.locationid ";
+
+//     client.query(thisAAQuery, (err, results) => {
+//         if (err) {throw err}
+//         else {
+//             console.log(res.rows);
+//             // do I need the var aaData below?
+//             // var aaData = JSON.stringify(res.rows)
+
+//     var output = {};
+
+//             fs.readFile('./aamap.html', 'utf8', (error, data) => {
+//                 var template = handlebars.compile(data);
+//                 output.meetings = results.rows
+//                 var html = template(output);
+//                 res.send(html);
+//             })
+//             client.end();
+//         }
+//     });
+// });
+
+
+
+//PROCESS BLOG
+
+app.get('/blog', function(req, res) {
+    
+    var dynamodb = new AWS.DynamoDB();
     
     var params = {
         TableName : "ProcessBlogAAY",
@@ -101,27 +158,32 @@ app.get('/blog', function(req, res) {
             ":maxDate": {S: new Date("2019-12-16").toISOString()}
         },
     };
+
     
     dynamodb.query(params, function(err, results) {
         if (err) {
             console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
         } else {
             console.log(results.rows);
-            fs.readFile('./blog.html', 'utf8', (error, data) => {
-                var template = handlebars.compile(data);
-                output.entries = results.Items
-                var html = template(output);
-                res.send(html);
-            })
-            console.log(results.Items)
+            
+            res.send(results.Items);
+            
+            // fs.readFile('./blog.html', 'utf8', (error, data) => {
+            //     var template = handlebars.compile(data);
+            //     output.entries = results.Items
+            //     var html = template(output);
+            //     res.send(html);
+            // })
+            // console.log(results.Items)
         }
     });
 
 });
 
+    // var output = {title: 'Process Blog', body: 'example content'};
+  
 
-
-// Temperature Sensor
+// TEMP SENSOR
 app.get('/sensor', function(req, res) {
     var output = {title: 'Temperature Sensor Visualization', body: 'example content'};
 
@@ -156,3 +218,24 @@ app.get('/sensor', function(req, res) {
 //              GROUP BY sensorday <br>
 //              ORDER BY sensorday;</p>`);
 // }); 
+
+
+// var landingPage = `<h1>Welcome to my three projects!</h1>
+// <ul>
+// <li><a href="/sensor">Sensor Data.</a></li>
+// <li><a href="/ProcessBlogAAY">Process Blog Data.</a></li>
+// <li><a href="/aamap">AA Map Data.</a></li>
+// </ul>
+// `;
+
+
+
+// serve static files in /public
+app.use(express.static('public'));
+
+// listen on port 8080
+app.listen(8080, function() {
+    console.log('Server listening...');
+});
+
+
